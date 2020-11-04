8b6ca95c370419b9f397aadd6d01072d0059ff82
5ock <ccrubio5@gmail.com>
2020-11-04 11:06:45 +0800

    Updates

---
+++ precache-manifest.ead78f193b96bbe1a0b99cdbf949b52c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d978eaa52f37995c0b6bd052baf2f35",
+    "url": "/booking-hotel/index.html"
+  },
+  {
+    "revision": "4fefc14f982222cfba84",
+    "url": "/booking-hotel/static/css/2.a3735241.chunk.css"
+  },
+  {
+    "revision": "1b84e1a39cb74d450a7c",
+    "url": "/booking-hotel/static/css/main.e78db26b.chunk.css"
+  },
+  {
+    "revision": "4fefc14f982222cfba84",
+    "url": "/booking-hotel/static/js/2.239ecfe2.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/booking-hotel/static/js/2.239ecfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b84e1a39cb74d450a7c",
+    "url": "/booking-hotel/static/js/main.267ee960.chunk.js"
+  },
+  {
+    "revision": "311be6d9316e11df4811",
+    "url": "/booking-hotel/static/js/runtime-main.78dd2460.js"
+  },
+  {
+    "revision": "44d3ea03aa93e4dbb1f66fed922ae13d",
+    "url": "/booking-hotel/static/media/air-conditioner.44d3ea03.svg"
+  },
+  {
+    "revision": "3fde7209c4392509c6472c9c7c7919c5",
+    "url": "/booking-hotel/static/media/breakfast.3fde7209.svg"
+  },
+  {
+    "revision": "380c9d2dd10f79d19933ebd9422ecf91",
+    "url": "/booking-hotel/static/media/check-mark.380c9d2d.svg"
+  },
+  {
+    "revision": "a0009ce2d8ca482417ac4502faff908c",
+    "url": "/booking-hotel/static/media/cross-mark.a0009ce2.svg"
+  },
+  {
+    "revision": "8148f2eccd141f7156d5771e6ee6caa2",
+    "url": "/booking-hotel/static/media/cross.8148f2ec.svg"
+  },
+  {
+    "revision": "2ff3ec148b822e6eb58e61d2c761c335",
+    "url": "/booking-hotel/static/media/document.2ff3ec14.svg"
+  },
+  {
+    "revision": "5e398a5d82e6e9853ff2f2ffa0721ec7",
+    "url": "/booking-hotel/static/media/error.5e398a5d.svg"
+  },
+  {
+    "revision": "e91f32678dcc3409fba4ddff211f6cd7",
+    "url": "/booking-hotel/static/media/fridge.e91f3267.svg"
+  },
+  {
+    "revision": "adb907afab4695da74c12928d996361c",
+    "url": "/booking-hotel/static/media/home_01.adb907af.jpg"
+  },
+  {
+    "revision": "4dc067fdf32f5963ef34ea2c5c5ec13b",
+    "url": "/booking-hotel/static/media/home_02.4dc067fd.jpg"
+  },
+  {
+    "revision": "89018568815a95ba1cb274211b655b90",
+    "url": "/booking-hotel/static/media/home_03.89018568.jpg"
+  },
+  {
+    "revision": "640c83f56512295df63093ba4f1c8f09",
+    "url": "/booking-hotel/static/media/home_04.640c83f5.jpg"
+  },
+  {
+    "revision": "294569f34e7b408e68bfa930ea6c0606",
+    "url": "/booking-hotel/static/media/left-arrow.294569f3.svg"
+  },
+  {
+    "revision": "3fafe7076ef3b04d64a9e3819ec2d7e9",
+    "url": "/booking-hotel/static/media/logo.3fafe707.svg"
+  },
+  {
+    "revision": "eb2d3d9b4db2c643a4c5be6bdc1de996",
+    "url": "/booking-hotel/static/media/mini-bar.eb2d3d9b.svg"
+  },
+  {
+    "revision": "c065be9da41e3801035ce5c5264d1c17",
+    "url": "/booking-hotel/static/media/no-smoking.c065be9d.svg"
+  },
+  {
+    "revision": "97a79f42be1b70c06e4231e89bd7b9dc",
+    "url": "/booking-hotel/static/media/payment.97a79f42.svg"
+  },
+  {
+    "revision": "4d86912fd720efbb4b65a53ac2ed2d3a",
+    "url": "/booking-hotel/static/media/pets.4d86912f.svg"
+  },
+  {
+    "revision": "6dc2081cb75ecf11be96e7624659c09b",
+    "url": "/booking-hotel/static/media/phone.6dc2081c.svg"
+  },
+  {
+    "revision": "f872bd553b493441bd191d557a132d16",
+    "url": "/booking-hotel/static/media/playground.f872bd55.svg"
+  },
+  {
+    "revision": "0c1c071da160caec83cda22308ae074d",
+    "url": "/booking-hotel/static/media/right-arrow.0c1c071d.svg"
+  },
+  {
+    "revision": "67cc65b8ec00833643972010eb205abf",
+    "url": "/booking-hotel/static/media/room-service.67cc65b8.svg"
+  },
+  {
+    "revision": "dbfedeff12444bfaff93f3ce123869f7",
+    "url": "/booking-hotel/static/media/sms.dbfedeff.svg"
+  },
+  {
+    "revision": "5a5f72ac3b989fc4023ca090558685f6",
+    "url": "/booking-hotel/static/media/sofa.5a5f72ac.svg"
+  },
+  {
+    "revision": "e8fb398559464ee08cd51575fa99a5ab",
+    "url": "/booking-hotel/static/media/success.e8fb3985.svg"
+  },
+  {
+    "revision": "7e153fd142ae978505cb283b96d4ee5a",
+    "url": "/booking-hotel/static/media/view.7e153fd1.svg"
+  },
+  {
+    "revision": "e84568fad0776113ea901c4bb26bd93e",
+    "url": "/booking-hotel/static/media/white-cross.e84568fa.svg"
+  },
+  {
+    "revision": "a2e1d2f93143d0ed7d1b6a7ddc812440",
+    "url": "/booking-hotel/static/media/wifi.a2e1d2f9.svg"
+  }
+]);
